75a6cb8a5065733752aa0b51885923fda6f2ecb1
stanlag <stanlag@mail.ru>
2024-02-21 16:17:02 +0300

    task_11_assignment_color_palette

---
+++ task_11_assignment_color_palette/lib/model/palette_utils_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:task_11_assignment_color_palette/assets/colors/color_palette.dart';
+import 'package:task_11_assignment_color_palette/entity/list_colors.dart';
+import 'package:task_11_assignment_color_palette/resources/resources.dart';
+import 'package:task_11_assignment_color_palette/resources/text.dart';
+
+abstract class PaletteUtilsModel {
+  static final List<ColorFromThePalette> listColor =
+      (dataColors['colors'] as List<Map<String, String>>)
+          .where((colorData) => colorData.containsKey('value'))
+          .toList()
+          .map((color) => ColorFromThePalette(color['name']??'', color['value']??'' ))
+          .toList();
+
+  static ColorFromThePalette getColorFromHex(ColorFromThePalette listColor) {
+    String hexColor = listColor.value.replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF$hexColor';
+    }
+    try {
+      listColor.color = Color(int.parse(hexColor, radix: 16));
+    } catch (error) {
+      listColor = ColorFromThePalette(listColor.name, TextApp.error);
+      listColor.color = Colors.black;
+    }
+    return listColor;
+  }
+
+  static void copyToClipboard(String text) {
+    Clipboard.setData(ClipboardData(text: text)).then((_) {
+      Fluttertoast.showToast(
+        msg: TextApp.hexCopy,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: AppColor.black80,
+        fontSize: 16.0,
+      );
+    });
+  }
+}
